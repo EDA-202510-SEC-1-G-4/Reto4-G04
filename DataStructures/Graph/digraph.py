e95d368d985dc5848a776df3ee7344f28d439e47
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-05-24 10:35:23 -0500

    aa

--- DataStructures/Graph/digraph.py
+++ DataStructures/Graph/digraph.py
@@ -19,6 +19,7 @@ def update_vertex_info(graph,key,info): #da.rincon
     vertice = mp.get(vertices,key)
     vertex.set_value(vertice,info)
     return graph
+
 def remove_vertex(graph,key):
     if graph != None and key != None:
         info_eliminado = mp.get(graph['vertices'],key) 
