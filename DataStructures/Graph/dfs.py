f64908354570b5aed2d1606363964897763f20b0
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-05-24 13:30:39 -0500

    dfs duro

--- DataStructures/Graph/dfs.py
+++ DataStructures/Graph/dfs.py
@@ -1,77 +1,71 @@
 from DataStructures.List import array_list as al
+from DataStructures.Map import map_linear_probing as mp
+from DataStructures.Queue import queue as q
 from DataStructures.Stack import stack as s
 from DataStructures.Graph import digraph as G
-from DataStructures.Map import map_linear_probing as mp
-'''''
-def dfs(grafo,source):
-    visited = al.new_list()
-    stack = s.new_stack()
-    s.push(stack,source)
-    while s.size(stack) > 0:
-        elem = s.pop(stack)
-        if elem not in visited['elements']:
-            al.add_last(visited,elem)
-            for nodo in G.adjacents(grafo,elem)['elements']:
-                if nodo not in visited['elements']:
-                    s.push(stack,nodo)
-    return visited
-    '''''
-def dfs(graph, source):
-    if not G.contains_vertex(graph, source):
-        raise Exception(f"El vértice '{source}' no existe en el grafo.")
 
-    visited = mp.new_map(10)
-    pre = al.new_list()
-    post = al.new_list()
-    reversepost = al.new_list()
+def dfs(my_graph, source):
+    """
+    Inicia el recorrido DFS desde el vértice source.
+    Retorna la estructura de búsqueda DFS con visited y edge_to.
+    """
+    visited = mp.new_map(100)
 
-    mp.put(visited, source, {'marked': True, 'edge_from': None})
-    dfs_vertex(graph, source, visited, pre, post, reversepost)
+    mp.put(visited, source, {
+        "marked": True,
+        "edge_to": None
+    })
 
-    return {
+    search = {
         'source': source,
-        'visited': visited,
-        'pre': pre,
-        'post': post,
-        'reversepost': reversepost
+        'visited': visited
     }
 
-def dfs_vertex(graph, vertex_key, visited, pre, post, reversepost):
-    al.add_last(pre, vertex_key)
+    dfs_vertex(my_graph, source, search)
+    return search
 
-    if not G.contains_vertex(graph, vertex_key):
-        if not vertex_key == "elements":
-            
-            raise Exception(f"Vértice '{vertex_key}' no encontrado al intentar obtener adyacentes.")
-
-    for neighbor in G.adjacents(graph, vertex_key):
-        if not mp.contains(visited, neighbor):
-            mp.put(visited, neighbor, {'marked': True, 'edge_from': vertex_key})
-            dfs_vertex(graph, neighbor, visited, pre, post, reversepost)
-
-    al.add_last(post, vertex_key)
-    al.add_first(reversepost, vertex_key)
-
-def has_path_to(vertex_key, search):
+def dfs_vertex(my_graph, vertex, search):
     """
-    Retorna True si hay un camino desde el vértice fuente a vertex_key.
+    Recorre el grafo desde vertex de forma recursiva usando DFS.
+    Marca cada vértice y guarda su predecesor (edge_to).
     """
-    return mp.contains(search['visited'], vertex_key)
+    visited = search['visited']
 
-def path_to(search, vertex_key):
+    entry = mp.get(my_graph['vertices'], vertex)
+    if entry is None:
+        raise Exception(f"Vértice '{vertex}' no encontrado en el grafo.")
+
+    adj_map = entry["adjacents"]
+    adj_keys = mp.key_set(adj_map)
+
+    for key in adj_keys["elements"]:
+        if not mp.contains(visited, key):
+            mp.put(visited, key, {
+                "marked": True,
+                "edge_to": vertex
+            })
+            dfs_vertex(my_graph, key, search)
+
+def has_path_to(search, vertex):
     """
-    Reconstruye el camino desde el vértice fuente hasta vertex_key.
+    Retorna True si hay camino desde search['source'] hasta vertex.
     """
-    if not has_path_to(vertex_key, search):
+    return mp.contains(search['visited'], vertex)
+
+def path_to(search, vertex):
+    """
+    Retorna una pila con el camino desde source hasta vertex.
+    Si no existe camino, retorna None.
+    """
+    if not has_path_to(search, vertex):
         return None
 
-    path = []
-    current = vertex_key
+    path = s.new_stack()
+    current = vertex
+
     while current != search['source']:
-        path.insert(0, current)
-        current = mp.get(search['visited'], current)['edge_from']
-    
-    path.insert(0, search['source'])
+        s.push(path, current)
+        current = mp.get(search['visited'], current)['value']['edge_to']
+
+    s.push(path, search['source'])
     return path
-
-
